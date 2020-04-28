289c80e2abfe7b5abd86370bc359722ccfc66f9b
Xavi Arias Seguí <876579+xaviarias@users.noreply.github.com>
2020-04-28 10:25:37 +0100

    Log4j to SLF4J (#50)
    * Migrate to SLF4J
    
    * Remove IDE warnings

--- src/main/kotlin/org/web3j/evm/EmbeddedEthereum.kt
+++ src/main/kotlin/org/web3j/evm/EmbeddedEthereum.kt
@@ -15,7 +15,6 @@ package org.web3j.evm
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.google.common.io.Resources
-import org.apache.logging.log4j.LogManager
 import org.hyperledger.besu.config.GenesisConfigFile
 import org.hyperledger.besu.config.JsonUtil
 import org.hyperledger.besu.crypto.SECP256K1
@@ -55,6 +54,7 @@ import org.hyperledger.besu.metrics.noop.NoOpMetricsSystem
 import org.hyperledger.besu.services.kvstore.InMemoryKeyValueStorage
 import org.hyperledger.besu.util.bytes.BytesValue
 import org.hyperledger.besu.util.uint.UInt256
+import org.slf4j.LoggerFactory
 import org.web3j.protocol.core.methods.response.EthBlock
 import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.utils.Numeric
@@ -492,7 +492,7 @@ class EmbeddedEthereum(configuration: Configuration, private val operationTracer
     }
 
     companion object {
-        private val LOG = LogManager.getLogger()
+        private val LOG = LoggerFactory.getLogger(EmbeddedEthereum::class.java)
 
         private fun hexToULong(hex: String): Long {
             return UInt256.fromHexString(hex).toLong()
